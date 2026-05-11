ae9874f7984e604cc5b6ddfe9ad8801e32c52232
kuangkaize <kuangkaize@graduate.utm.my>
2026-05-11 15:00:32 +0800

    update db

--- database/facility_booking_system.sql
+++ database/facility_booking_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2026 at 02:18 PM
+-- Generation Time: May 11, 2026 at 09:00 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,8 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `role` varchar(20) NOT NULL,
+  `role` varchar(20) DEFAULT 'student',
+  `full_name` varchar(100) NOT NULL,
+  `user_id` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -39,8 +42,11 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `role`, `email`, `password`, `created_at`) VALUES
-(1, 'student', 'student@gmail.com', '123456', '2026-05-09 05:59:39');
+INSERT INTO `users` (`id`, `role`, `full_name`, `user_id`, `email`, `phone`, `password`, `created_at`) VALUES
+(1, 'student', 'John Tan', 'S12345', 'student@gmail.com', '0123456789', '123456', '2026-05-11 06:33:58'),
+(2, 'staff', 'Mr Lim', 'ST001', 'staff@gmail.com', '0112233445', '123456', '2026-05-11 06:33:58'),
+(3, 'admin', 'System Admin', 'AD001', 'admin@gmail.com', '0109988776', '123456', '2026-05-11 06:33:58'),
+(5, 'student', 'zzz', 'zzz', 'zzz@gmail.com', '', '$2y$10$kzHjVKrZnE5eHD3MDUI8/uaO.nLxvo2.LjO40VEUoIWIhXGyuO3yy', '2026-05-11 06:53:28');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
